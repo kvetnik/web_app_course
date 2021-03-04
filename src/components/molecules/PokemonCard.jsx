3cb526c127ccbbf690f00de098235182ddbefece
kvetnik <58985912+kvetnik@users.noreply.github.com>
2021-03-04 14:04:13 +0300

    commit

--- src/components/molecules/PokemonCard.jsx
+++ src/components/molecules/PokemonCard.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import {PokemonImage} from "../atoms/PokemonImage";
 import {PokemonName} from "../atoms/PokemonName";
-import {CaughtButton} from "../atoms/CaughtButton";
+// import {CaughtButton} from "../atoms/CaughtButton";
 import {Link} from "react-router-dom";
 import {Card, Container} from "react-bootstrap";
 
